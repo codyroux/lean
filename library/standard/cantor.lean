cf8a27611a54c55e1a95096fabac5a948bf0b18f
Cody Roux <croux@cmu.edu>
2014-07-27 22:05:39 +0200

    Begin theorem injective <-> retract

--- library/standard/cantor.lean
+++ library/standard/cantor.lean
@@ -2,7 +2,7 @@
 -- Released under Apache 2.0 license as described in the file LICENSE.
 -- Author: Cody Roux
 
-import logic
+import logic classical
 -- using eq_proofs
 
 abbreviation subsets (P : Type) := P → Prop.
@@ -16,6 +16,7 @@ hypothesis r : A → subsets A.
 hypothesis i : subsets A → A.
 
 hypothesis retract {P : subsets A} {a : A} : r (i P) a = P a.
+-- hypothesis retract_r : retract r i.
 
 definition delta (a:A) : Prop := ¬ (r a a).
 
@@ -23,7 +24,7 @@ notation `δ` := delta.
 
 hypothesis P : Prop.
 
-check eq_rec.
+-- check eq_rec.
 
 -- Crashes unifier!
 -- theorem false_aux : ¬ (δ (i delta))
@@ -64,10 +65,47 @@ end
 
 hypothesis I : Type.
 
-definition F (X : Type) : Type := (X → Prop) → Prop.
+-- definition F (X : Type) : Type := (X → Prop) → Prop.
+hypothesis F.{l} (X : Type.{l}) : Type.{l}.
+
+-- set_option pp.universes true.
+
+check F.
+
+
+-- check F.
 
 hypothesis unfold : I → F I.
+check unfold.
 hypothesis fold   : F I → I.
-
+check fold.
 hypothesis iso1 : ∀ x : I, fold (unfold x) = x.
 hypothesis iso2 : ∀ x : F I, unfold (fold x) = x.
+
+section
+
+definition retract {A B : Type} (f : A → B) (g : B → A) := ∀ a : A, g (f a) = a.
+
+definition inj {A B : Type} (f : A → B):= ∀ x y : A, f x = f y → x = y.
+
+check case.
+
+definition ite {A : Type} : Prop → A → A → A := λ P a b, case _ a b P.
+
+axiom sorry (t : Type) : t.
+
+check exists_intro.
+
+check inhabited_elim.
+
+definition image {A B : Type} (f : A → B) : B → Prop := λ y, ∃ x : A, f x = y.
+
+theorem inj_is_retract : ∀ (A B : Type) (f : A → B), inhabited A → inj f → ∃ g, retract f g
+:= assume A B : Type,
+   assume f : A → B,
+   assume H : inhabited A,
+   assume inj_f : inj f,
+          inhabited_elim H
+          (assume a : A,
+           let g := sorry (B → A) in
+           exists_intro g _).
