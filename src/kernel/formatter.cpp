4e147700bb24af65130c4f2574b519f3c84ed673
Leonardo de Moura <leonardo@microsoft.com>
2014-08-01 15:07:01 -0700

    fix(kernel/formatter): fixes #21
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- src/kernel/formatter.cpp
+++ src/kernel/formatter.cpp
@@ -16,9 +16,30 @@ void set_print_fn(std::function<void(std::ostream &, expr const &)> const & fn) 
     g_print = new std::function<void(std::ostream &, expr const &)>(fn);
 }
 
-std::ostream & operator<<(std::ostream & out, expr const & e) {
-    if (g_print) {
-        (*g_print)(out, e);
+bool is_internal_name(name n) {
+    while (!n.is_atomic())
+        n = n.get_prefix();
+    return n.is_numeral();
+}
+
+static name g_x("x");
+std::pair<expr, expr> binding_body_fresh(expr const & b, bool preserve_type) {
+    lean_assert(is_binding(b));
+    name n = binding_name(b);
+    if (is_internal_name(n))
+        n = g_x;
+    n = pick_unused_name(binding_body(b), n);
+    expr c = mk_local(n, preserve_type ? binding_domain(b) : expr());
+    return mk_pair(instantiate(binding_body(b), c), c);
+}
+
+static name g_internal("M");
+name fix_internal_name(name const & a) {
+    if (a.is_atomic()) {
+        if (a.is_numeral())
+            return g_internal;
+        else
+            return a;
     } else {
         throw exception("print function is not available, Lean was not initialized correctly");
     }
